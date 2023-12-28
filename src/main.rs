c1ed39683f5354ee97c114c04ba77440262db50b
pu <nampu@finema.co>
2023-12-28 22:20:04 +0700

    save

--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,10 @@ async fn main() {
     let app = Router::new()
         .route("/health", get(get_health))
         .route("/testquery/:user_id", get(get_test_query))
+        .route("/testquerymac/:user_id", get(get_test_query_mac))
         .route("/adduser", post(post_test_query))
         .route("/deleteallusers", delete(delete_all_users))
+        .route("/checkexisting", post(post_check_existing))
         .layer(TimeoutLayer::new(Duration::from_secs(30)))
         .with_state(pool);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -39,9 +41,6 @@ async fn get_health(State(pool): State<PgPool>) -> StatusCode {
         Ok(_) => StatusCode::OK,
         Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
     }
-}
-
-async fn get_test_query(Path(user_id): Path<i64>, State(pool): State<PgPool>) -> impl IntoResponse {
 }
 
 async fn post_test_query(
@@ -76,6 +75,51 @@ async fn delete_all_users(
         }
     } else {
         StatusCode::UNAUTHORIZED
+    }
+}
+
+async fn get_test_query(
+    Path(user_id): Path<i64>,
+    State(pool): State<PgPool>,
+) -> Result<String, StatusCode> {
+    let row: Result<(String,), sqlx::Error> =
+        sqlx::query_as("SELECT email FROM users WHERE id = $1")
+            .bind(user_id)
+            .fetch_one(&pool)
+            .await;
+    match row {
+        Ok(r) => Ok(r.0),
+        Err(_) => Err(StatusCode::NO_CONTENT),
+    }
+}
+
+async fn get_test_query_mac(
+    Path(user_id): Path<i64>,
+    State(pool): State<PgPool>,
+) -> Result<String, StatusCode> {
+    let row = sqlx::query!("SELECT email FROM users WHERE id = $1", user_id)
+        .fetch_one(&pool)
+        .await;
+
+    match row {
+        Ok(r) => Ok(r.email),
+        Err(_) => Err(StatusCode::NO_CONTENT),
+    }
+}
+
+async fn post_check_existing(
+    State(pool): State<PgPool>,
+    Json(payload): Json<Users>,
+) -> impl IntoResponse {
+    let row = sqlx::query("SELECT * FROM public.users WHERE email = $1 OR email_secondary = $2")
+        .bind(&payload.email)
+        .bind(&payload.email)
+        .fetch_one(&pool)
+        .await;
+
+    match row {
+        Ok(_) => "already taken",
+        Err(_) => "available",
     }
 }
 
