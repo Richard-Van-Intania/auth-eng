b951c63ea70207c9a7404ba06c941d7d673a7efd
pu <nampu@finema.co>
2023-12-31 22:53:04 +0700

    save

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,36 @@
-use axum::{routing::get, Router};
+use axum::{extract::State, response::IntoResponse, routing::get, Router};
+use elasticsearch::{Elasticsearch, SearchParts};
+use serde_json::json;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/health", get(|| async {}));
+    let client = Elasticsearch::default();
+    let app = Router::new()
+        .route("/health", get(|| async {}))
+        .route("/testelasticsearch", get(test_elasticsearch))
+        .with_state(client);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn test() {}
+
+async fn test_elasticsearch(State(client): State<Elasticsearch>) -> impl IntoResponse {
+    let response = client
+        .search(SearchParts::Index(&["books"]))
+        .from(0)
+        .size(10)
+        .body(json!({
+            "query": {
+                "match": {
+                    "name": "brave"
+                }
+            }
+        }))
+        .send()
+        .await;
+    match response {
+        Ok(_) => "ok".to_string(),
+        Err(e) => e.to_string(),
+    }
+}
